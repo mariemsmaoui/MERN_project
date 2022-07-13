f1d29033822edc7e5d137eb110e43405a9697ed9
mariem <sm.mariem99@gmail.com>
2022-07-13 21:32:14 +0100

    commit this

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,28 @@
-import express, { response } from 'express'
-import data from './data.js';
+import express, { response } from "express";
+import data from "./data.js";
 
 const app = express();
-app.get('/api/products',(req,res)=>{
-  res.send(data.products)})
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
 
-app.get('/api/products/slug/:slug',(req,res)=>{
-  const product = data.products.find(x=>x.slug === req.params.slug)
-  if(product){
-    res.send(product)
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "product Not Found" });
   }
-  else{
-    res.status(404).send({message:'product Not Found'})
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params._id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "product Not Found" });
   }
- 
-
-})
-app.get('/api/products/slug/:slug',(req,res)=>{
-  const product = data.products.find(x=>x._id === req.params._id)
-  if(product){
-    res.send(product)
-  }
-  else{
-    res.status(404).send({message:'product Not Found'})
-  }
- 
-
-})
-const port =process.env.PORT || 5000;
-app.listen(port , ()=>{
-  console.log(`serve at http://localhost:${port}`)
-})
-
-
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,30 +1,23 @@
-import { createContext, useReducer } from 'react';
+import { createContext,useReducer } from "react";
 
-export const Store = createContext();
-
-const initialState = {
-  cart: {
-    cartItems: [],
-  },
-};
-function reducer(state, action) {
-  switch (action.type) {
-    case 'CART_ADD_ITEM':
-      // add to cart
-      return {
-        ...state,
-        cart: {
-          ...state.cart,
-          cartItems: [...state.cart.cartItems, action.payload],
-        },
-      };
-    default:
-      return state;
-  }
+const intialState ={
+    cart : {
+        cartItems: [],
+    },
 }
-
-export function StoreProvider(props) {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const value = { state, dispatch };
-  return <Store.Provider value={value}>{props.children} </Store.Provider>;
+export const Store= createContext();
+function reducer(state,action){
+    switch(action.type){
+    case'CART_ADD_ITEM':
+    //add to cart
+    return{...state, cart :{...state.cart, cartItems:[...state.cart.cartItems]}}
+    default:
+        return state;
+    }
+}
+}
+export function Storeprovider(props){
+   const [state, dispatch] = useReducer(reducer, intialState);
+   const value ={state,dispatch}
+   return <Store.provider value ={value}>{props.children}</Store.provider>
 }
